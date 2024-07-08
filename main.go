c6b4db299b49cf50a79e214523b46746ccd240b4
Tubelight30 <soumaygupta2004@gmail.com>
2024-07-08 21:42:53 +0530

    Update main.go to use a config struct for nextLocationAreaURL and prevLocationAreaURL

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
+type config struct {
+	nextLocationAreaURL *string
+	prevLocationAreaURL *string
+}
+
 func main() {
-	startRepl()
+
+	cfg := config{
+		nextLocationAreaURL: nil,
+		prevLocationAreaURL: nil,
+	}
+	startRepl(&cfg)
+
+	// ticker := time.NewTicker(500 * time.Millisecond)
+	// for {
+
+	// 	t := <-ticker.C
+	// 	fmt.Println(t)
+	// }
+
 }
