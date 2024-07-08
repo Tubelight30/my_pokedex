5080b8541bc0b5c5c87186b94a06f6d4db70200f
Tubelight30 <soumaygupta2004@gmail.com>
2024-07-08 21:45:02 +0530

    commandMap and commandMapb functions to use pokeapi.ListLocationAreas

---
+++ command_map.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/Tubelight30/my_pokedex/internal/pokeapi"
+)
+
+func commandMap(c *config) error {
+
+	resp, err := pokeapi.ListLocationAreas(c.nextLocationAreaURL)
+	if err != nil {
+		// log.Fatal(err)
+		return err
+	}
+	for _, area := range resp.Results {
+		fmt.Println(area.Name)
+	}
+	// fmt.Println(resp)
+	c.nextLocationAreaURL = resp.Next
+	c.prevLocationAreaURL = resp.Previous
+	return nil
+}
+
+func commandMapb(c *config) error {
+	if c.prevLocationAreaURL == nil {
+		return errors.New("you are on the first page")
+	}
+	resp, err := pokeapi.ListLocationAreas(c.prevLocationAreaURL)
+	if err != nil {
+		return err
+	}
+	for _, area := range resp.Results {
+		fmt.Println(area.Name)
+	}
+	c.nextLocationAreaURL = resp.Next
+	c.prevLocationAreaURL = resp.Previous
+	return nil
+}
